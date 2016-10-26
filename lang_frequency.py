ae62131a67f8912edf843d19054ede3bf827648a
Alexey <beastrock@mail.ru>
2016-10-26 19:50:03 +0300

    Create script, all functions are clear, lunching by sysargv

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,30 @@
+import collections
+import re
+import requests
+import sys
 
 
-def load_data(filepath):
-    pass
+def load_data(file_location):
+    if '://' in file_location:
+        r = requests.get(file_location)
+        text = r.text
+        return text
+    else:
+        with open(file_location, 'r', encoding='utf-8') as f:
+            text = f.read()
+            return text
 
 
 def get_most_frequent_words(text):
-    pass
+    lower_text = text.lower()
+    list_of_words = re.split('\W*\s\W*', lower_text)
+    print(list_of_words)
+    dict_of_words = collections.Counter(list_of_words)
+    for word, count in dict_of_words.most_common(10):
+        print('%s : %5d' % (word, count))
+    return
 
 
 if __name__ == '__main__':
-    pass
+    file_location = sys.argv[1]
+    get_most_frequent_words(load_data(file_location))
